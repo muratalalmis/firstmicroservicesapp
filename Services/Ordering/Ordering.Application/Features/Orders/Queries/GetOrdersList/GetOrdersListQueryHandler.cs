7494fc4a5421882a5b539658a3cb575a8177923e
Murat <murat@Murat-MacBook-Pro.local>
2022-11-20 22:09:35 +0300

    Before of features

---
+++ Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQueryHandler : IRequestHandler<GetOrdersListQuery, List<OrdersVm>>
+    {
+        private readonly IOrderRepository orderRepository;
+        private readonly IMapper mapper;
+
+        public GetOrdersListQueryHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            this.orderRepository = orderRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task<List<OrdersVm>> Handle(GetOrdersListQuery request, CancellationToken cancellationToken)
+        {
+            var orderList = await orderRepository.GetOrdersByUserName(request.UserName);
+            return mapper.Map<List<OrdersVm>>(orderList);
+        }
+    }
+}
+
