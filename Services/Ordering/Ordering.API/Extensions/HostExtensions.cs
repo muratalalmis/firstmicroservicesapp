b5b84c2a682cd0975de2b762916b81a854495e02
Murat <murat@Murat-MacBook-Pro.local>
2022-11-27 16:34:15 +0300

    create table to azure sql

--- Services/Ordering/Ordering.API/Extensions/HostExtensions.cs
+++ Services/Ordering/Ordering.API/Extensions/HostExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Runtime.ExceptionServices;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -18,7 +20,57 @@ namespace Ordering.API.Extensions
             {
                 var services = scope.ServiceProvider;
                 var logger = services.GetRequiredService<ILogger<TContext>>();
+                var configuration = services.GetRequiredService<IConfiguration>();
                 var context = services.GetService<TContext>();
+
+                var connectionString = configuration.GetConnectionString("OrderingConnectionString");
+                try
+                {
+                    var masterConnectionString = connectionString.Replace("Database=OrderDb", "Database=master");
+                    RunSqlCommand(masterConnectionString, "Create Database OrderDb");
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "An unhandled error occurred.");
+                }
+
+                try
+                {
+                    RunSqlCommand(connectionString, @"
+
+CREATE TABLE Orders(
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+    [UserName] [nvarchar](75) NOT NULL,
+	[TotalPrice] [decimal] NOT NULL,
+
+    [FirstName] [nvarchar](75) NULL,
+    [LastName] [nvarchar](75) NULL,
+    [EmailAddress] [nvarchar](75) NULL,
+    [AddressLine] [nvarchar](75) NULL,
+    [Country] [nvarchar](75) NULL,
+    [State] [nvarchar](75) NULL,
+    [ZipCode] [nvarchar](75) NULL,
+    [CardName] [nvarchar](75) NULL,
+    [CardNumber] [nvarchar](75) NULL,
+    [Expiration] [nvarchar](75) NULL,
+    [CVV] [nvarchar](75) NULL,
+	[PaymentMethod] [int] NULL,
+
+	[CreatedBy] [nvarchar](75) NOT NULL,
+	[CreatedDate] [DateTime] NOT NULL,
+	[LastModifiedBy] [nvarchar](75) NULL,
+	[LastModifiedDate] [DateTime] NULL
+
+ CONSTRAINT [PK_dbo.Orders] PRIMARY KEY CLUSTERED 
+        ([Id] ASC )
+        WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+");
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "An unhandled error occurred.");
+                }
 
                 try
                 {
@@ -50,6 +102,16 @@ namespace Ordering.API.Extensions
             // TODO : Code first migrations not supported on azure sql edge
             //context.Database.Migrate();
             seeder(context, services);
+        }
+
+        private static void RunSqlCommand(string connectionString, string queryString)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var command = new SqlCommand(queryString, connection);
+                connection.Open();
+                var result = command.ExecuteNonQuery();
+            }
         }
     }
 }
