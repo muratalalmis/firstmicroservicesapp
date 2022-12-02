2124752ee7e9aecd6ff8aa33823fbf1f458b0486
Murat <murat@Murat-MacBook-Pro.local>
2022-12-02 13:44:19 +0300

    ocelot

--- ApiGateways/OcelotApiGw/Program.cs
+++ ApiGateways/OcelotApiGw/Program.cs
@@ -18,16 +18,20 @@ namespace OcelotApiGw
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                })
-                .ConfigureLogging((hostingContext, logginBuilder) =>
-                {
-                    logginBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-                    logginBuilder.AddConsole();
-                    logginBuilder.AddDebug();
-                });
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+            })
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            })
+            .ConfigureLogging((hostingContext, logginBuilder) =>
+            {
+                logginBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                logginBuilder.AddConsole();
+                logginBuilder.AddDebug();
+            });
     }
 }
 
